d3894d5bd2aef379d679f147cde64265862b55c2
Paulo Henrique Deléo <paulodeleo@gmail.com>
2022-02-28 11:28:51 -0300

    Mudado tamanho e cor do player

--- index.js
+++ index.js
@@ -9,11 +9,12 @@ class Player {
       x: 100,
       y: 100
     }
-    this.width = 100
-    this.height = 100
+    this.width = 30
+    this.height = 30
   }
 
   draw() {
+    c.fillStyle = 'red'
     c.fillRect(this.position.x, this.position.y, this.width, this.height)
   }
 }
